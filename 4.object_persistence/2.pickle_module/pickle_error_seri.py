2ad77b06bb3fdc73f0b22d8430530b56dcd41867
devisono <devison_santos@yahoo.com.br>
2024-08-06 15:38:06 -0300

    Pickle module

---
+++ 4.object_persistence/2.pickle_module/pickle_error_seri.py
@@ -0,0 +1,9 @@
+import pickle
+
+
+def f1():
+    print('Hello from the jar!')
+
+
+with open('function.pckl', 'wb') as file_out:
+    pickle.dump(f1, file_out)
---
+++ 4.object_persistence/2.pickle_module/pickle_error_seri_class.py
@@ -0,0 +1,15 @@
+import pickle
+
+
+class Cucumber:
+    def __init__(self):
+        self.size = 'small'
+
+    def get_size(self):
+        return self.size
+
+
+cucu = Cucumber()
+
+with open('cucumber.pckl', 'wb') as file_out:
+    pickle.dump(cucu, file_out)
